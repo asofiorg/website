a98cd70dab16530032d786212bb8d991f733545a
Juan Almanza <hi@scidroid.co>
2022-03-29 16:25:53 -0500

    feat: :lipstick: Added TailwindCSS
    This includes typography and theme modules

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "next-themes";
+
+import "styles/global.css";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;600&family=Space+Grotesk:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
