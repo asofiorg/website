ab8aaf52aa83f1e243655facce99dee7d90ce62d
Juan Almanza <hi@scidroid.co>
2022-11-24 01:57:41 -0500

    fix: error with unused dependencies

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,7 @@
-import { useTheme } from "next-themes";
-
 const Button = ({ className, children, ...props }) => {
-  const { theme } = useTheme();
-
   return (
     <button
-      className={`appearance-none font-mono font-semibold py-3 px-6 rounded-md border-2 bg-transparent text-lg cursor-pointer ${
-        theme == "dark" ? "button-dark" : "button"
-      } ${className}`}
+      className={`appearance-none font-mono font-semibold py-3 px-6 rounded-md border-2 bg-transparent text-lg cursor-pointer button ${className}`}
       {...props}
     >
       {children}
